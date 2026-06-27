755335cc56cb211be6a7968469617d306420d918
livinusonynso <livinusonyenso@gmail.com>
2026-06-27 14:00:14 +0100

    fix:update cv

--- components/header.tsx
+++ components/header.tsx
@@ -140,7 +140,7 @@ export default function Header() {
                 ))}
                 <li>
                   <Button size="sm" asChild className="w-full">
-                    <a href="/UGWUJA_LIVINUS_EKENE_CV.pdf" target="_blank" rel="noopener noreferrer">
+                    <a href="/ugwuja_livinus_ekene_cv.pdf" target="_blank" rel="noopener noreferrer">
                       Resume
                     </a>
                   </Button>
